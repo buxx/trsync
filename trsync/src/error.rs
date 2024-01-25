0377b27fdc8ff7c9e7ec2ba5e8faeafaca940afb
Bastien Sevajol <bastien.sevajol@algoo.fr>
2024-01-25 22:22:13 +0100

    chore: remove unused code

--- trsync/src/error.rs
+++ trsync/src/error.rs
@@ -1,62 +1,6 @@
-use std::{fmt, io, str::Utf8Error};
+use std::{io, str::Utf8Error};
 use strum_macros::Display;
 use thiserror::Error;
-
-use trsync_core::types::{AbsoluteFilePath, ContentId, RevisionId};
-
-// FIXME BS NOW : a remplacer par ce qu'il y a dans core
-#[derive(Debug, Clone)]
-pub enum ClientError {
-    InputFileError(AbsoluteFilePath),
-    RequestError(String),
-    UnexpectedResponse(String),
-    AlreadyExistResponse(ContentId, RevisionId),
-    AlreadyExistResponseAndFailToFoundIt(String),
-    NotFoundResponse(String),
-    DecodingResponseError(String),
-    NotRelevant(String),
-}
-
-impl From<reqwest::Error> for ClientError {
-    fn from(error: reqwest::Error) -> Self {
-        Self::RequestError(format!("Error happen when make request : {:?}", error))
-    }
-}
-
-impl From<Error> for ClientError {
-    fn from(error: Error) -> Self {
-        Self::RequestError(format!("Error happen when make request : {:?}", error))
-    }
-}
-
-impl fmt::Display for ClientError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let message = match self {
-            ClientError::InputFileError(absolute_file_path) => {
-                format!("Error when reading input file '{}'", absolute_file_path)
-            }
-            ClientError::RequestError(message) => {
-                format!("Error when making request : '{}'", message)
-            }
-            ClientError::UnexpectedResponse(message) => {
-                format!("UnExpected response : '{}'", message)
-            }
-            ClientError::AlreadyExistResponse(content_id, revision_id) => {
-                format!("Content already exist : '{}'({})", content_id, revision_id)
-            }
-            ClientError::AlreadyExistResponseAndFailToFoundIt(message) => format!(
-                "Already exist but fail to found remote content : '{}'",
-                message
-            ),
-            ClientError::NotFoundResponse(message) => format!("Not found : '{}'", message),
-            ClientError::DecodingResponseError(message) => {
-                format!("Decoding error : '{}'", message)
-            }
-            ClientError::NotRelevant(message) => format!("Note : '{}'", message),
-        };
-        write!(f, "{}", message)
-    }
-}
 
 // FIXME BS NOW : supprimer cet enum ?
 #[derive(Debug, Error, Display)]
@@ -76,15 +20,6 @@ impl Error {
         match self {
             Error::NotRelevant(_) => log::Level::Debug,
             _ => log::Level::Error,
-        }
-    }
-}
-
-impl From<ClientError> for Error {
-    fn from(err: ClientError) -> Self {
-        match err {
-            ClientError::NotRelevant(message) => Error::NotRelevant(message),
-            _ => Error::UnexpectedError(format!("{:?}", err)),
         }
     }
 }
